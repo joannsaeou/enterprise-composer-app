7b2460f9334f8cbb35e80ff2e456080536149747
Joann Saeou <joannssaeou@gmail.com>
2020-08-25 15:36:07 -0700

    initial push

---
+++ src/app/composer.interface.ts
@@ -0,0 +1,16 @@
+/**
+ * Title: composer.interface.ts
+ * Author: Joann Saeou
+ * Date: 29 July 2020
+ * Description: Demonstrating  export interface interface
+ */
+
+
+export interface IComposer {
+
+    composerId: number;
+    fullName: string;
+    genre: string;
+
+
+}
